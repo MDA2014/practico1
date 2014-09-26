e6e3f348ada60f9749c3384cd6c3bd10c1e46b3c
adamantike <adamantike@users.noreply.github.com>
2014-09-26 13:30:36 -0300

    Código inicial de la cátedra, con modificaciones

---
+++ src-gen/modelo/Alumno.java
@@ -0,0 +1,30 @@
+package modelo;
+
+public class Alumno
+
+{
+
+	// crear los atributos de la clase
+
+	// crear las asociaciones a otras clases e interfaces
+
+	// crear los punteros a otras clases por composicion
+
+	// crear los punteros a otras clases por agregacion
+
+	public Alumno() {
+	}
+
+	// crear getter y setter de atributos
+
+	// crear getter y setter de referencias de asociacion
+
+	// crear getter y setter de referencias de compoisicion
+
+	// crear getter y setter de referencias de agregacion
+
+	// mètodos de la clase
+
+	// mètodo redefinido de una he derencia clases
+
+}
